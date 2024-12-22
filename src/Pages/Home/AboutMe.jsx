007e7f61cfc7edfcc476744f74fac03bd248d54d
salah <akilsalaheddine203@gmail.com>
2024-12-22 21:04:01 +0100

    first commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,26 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about2.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          Passionate Full Stack Developer specializing in building web applications, 
+          with solid experience in front-end and back-end development. Skilled in HTML,
+          CSS, PHP, and Java, as well as frameworks such as Spring Boot, Laravel, Vue.js,
+          and Tailwind. Successfully completed multiple projects,gaining practical
+          expertise and delivering effective solutions to technical challenges. 
+          </p>
+          <p className="hero--section-description">
+          Committed to continuous learning, growth in software development,
+          and providing innovative solutions to today's technological challenges.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,23 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Akil Salah Eddine </p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+          Développement Full Stack pour des solutions fiables et évolutives qui transforment vos idées en réalité
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/profile-about.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
